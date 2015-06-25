4496d53fb0d3d6566733e2bbf8077352ccd0e702
Dan Austin <dja470@bham.ac.uk>
2015-06-25 14:15:14 +0100

    handle cloud storage of images

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'bootstrap-will_paginate', '0.0.10.0'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+gem 'fog', '1.23.0'
 # Use unicorn as the app server
 # gem 'unicorn'
 
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,7 +7,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  
+  if Rails.env.production?
+    storage :fog # Use fog for production 
+  else 
+    storage :file # Use local storage for dev
+  end
+
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
